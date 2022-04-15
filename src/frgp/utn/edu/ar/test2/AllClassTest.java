1f737f235b746180c54539bc0e4a2dc500898010
UTN-FRGP-2022-LabV <sergio.palacio@alumnos.frgp.utn.edu.ar>
2022-04-15 16:34:14 -0300

    Suit test 2 agregada

---
+++ src/frgp/utn/edu/ar/test2/AllClassTest.java
@@ -0,0 +1,13 @@
+package frgp.utn.edu.ar.test2;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.runner.RunWith;
+
+import frgp.utn.edu.ar.test.MatrizAdyacenciaTest;
+
+@RunWith(JUnitPlatform.class)
+@SelectClasses( MatrizAdyacenciaTest.class )
+public class AllClassTest {
+
+}
